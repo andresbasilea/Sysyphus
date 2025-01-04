a3bc4ec7891f0aff4fa22b1406c7d78e46f48138
andresbasilea <andresbasilealvarez@gmail.com>
2025-01-04 12:50:56 -0600

    Quartz sync: Jan 4, 2025, 12:50 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'andresbasilea/Sysyphus',
+        // from data-repo-id
+        repoId: 'R_kgDONRQ31g',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONRQ31s4CluPJ',
+      }
+    }),
+  ],,
   footer: Component.Footer({
     links: {
       //GitHub: "https://github.com/jackyzha0/quartz",
